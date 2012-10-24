d525264d4ea68d67a4347be9ca3a9589b1911414
Raul Sanchez <raul@um.es>
2012-10-24 19:49:39 +0200

    playing with user controller tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,13 @@ class UsersController < ApplicationController
   def update
     authorize! :update, @user, :message => 'Not authorized as an administrator.'
     @user = User.find(params[:id])
+    
+    # Can't remove admin role to admin
+    if (@user.has_role? (:admin) && (params[:user][:role_ids].include?("1"))) 
+      redirect_to user_path, :alert => "Unable to update user. Can't remove rol to admin."
+    end
+
+
     if @user.update_attributes(params[:user], :as => :admin)
       redirect_to users_path, :notice => "User updated."
     else
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,13 +2,13 @@ require 'spec_helper'
 
 describe UsersController do
 
-  before (:each) do
-    @user = FactoryGirl.create(:user)
-    sign_in @user
-  end
-
   describe "GET 'show'" do
-    
+
+    before (:each) do
+      @user = FactoryGirl.create(:user)
+      sign_in @user
+    end
+
     it "should be successful" do
       get :show, :id => @user.id
       response.should be_success
@@ -18,7 +18,60 @@ describe UsersController do
       get :show, :id => @user.id
       assigns(:user).should == @user
     end
-    
   end
 
+  describe "Update" do
+
+    context "admin rol"
+      before (:each) do
+        Role.create([
+          { :name => 'admin' }, 
+          { :name => 'company' }, 
+          { :name => 'user' }
+          ], :without_protection => true)
+
+        @admin = FactoryGirl.create(:user)
+        @admin.add_role :admin
+        @user = FactoryGirl.create(:user)
+        @user.add_role :user
+        sign_in @admin
+      end
+      
+      it "can change role to users" do
+        put :update, :id=> @user.id, :user => {:role_ids => '2'}
+        @user.has_role?(:company).should be_true
+      end
+
+
+      it "can change role to users" do
+        put :update, :id=> @user.id, :user => {:role_ids => '2'} 
+        @user.has_role?(:user).should be_false
+      end
+
+
+      it "can't delete admin role from himself" do
+        put :update, :id=> @admin.id, :user => {:role_ids => '2'}
+        @admin.has_role?(:admin).should be_true
+      end
+
+
+      it "can delete users" do
+        pending
+      end
+    end
+
+
+    context "Company Rol" do
+      it "can't change role to users" do
+        pending
+      end
+
+      it "can add regular users in his company" do
+        pending
+      end
+
+      it "can delete regular users of his company" do 
+        pending
+      end
+  end
 end
